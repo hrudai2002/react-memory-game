97508b5d736198153d90670519a5b02fbb3d28b1
sayihrudai <saihrudai11@gmail.com>
2022-05-30 11:20:14 +0530

    updated

--- src/components/SingleCard.js
+++ src/components/SingleCard.js
@@ -11,7 +11,7 @@ export default function SingleCard({ card, handleChoice, flipped, disabled}) {
         <img className="front" src={card.src} alt="card front" />
         <img
           className="back"
-          src="/img/cover.png"
+          src="/img/cover.jpg"
           onClick={handleClick}
           alt="card back"
         />
